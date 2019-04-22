369030e814e7c739fac7aa6fe871f168d17a77d9
Max <lol.frag.lol@gmail.com>
2019-04-22 12:06:42 +0300

    'kek2'

---
+++ script.js
@@ -0,0 +1,4 @@
+var start = document.getElementById('react-container');
+var kek = React.createElement("h1", {id: "kek", className: "kek"}, "Kek");
+var kek2 = React.createElement("p", null, "kek2");
+ReactDOM.render(kek, start);
